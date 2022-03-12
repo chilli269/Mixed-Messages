bc264c0ab5168c9dd31761233ed5cd58ca05d48d
Mihnea <grigoremihnea06@gmail.com>
2022-03-12 16:00:46 +0200

    Create mixedmessages.js

---
+++ mixedmessages.js
@@ -0,0 +1,34 @@
+const nouns=['after', 'birth', 'glow', 'aircraft', 'backpack', 'anyone', 'arrowhead',
+'backspin', 'stop', 'sign', 'below', 'bellbottom', 'blueberries', 'top', 'bodygurad',
+'park', 'track', 'mum', 'dad', 'Jack', 'Mike', 'Robin', 'Alex', 'He', 'She', 'Her', 
+'blabla', 'is', 'eat', 'drink', 'talk', 'say', 'want', 'need', 'play', 'let', 'go', 'drive' ];
+const numOfWords=()=>{
+    let a=Math.floor(Math.random()*10);
+    return a;
+}
+const createMessage=now=>{
+    if(now>0){
+        let sentence='';
+        for(let i=0;i<now;i++)
+        {
+            if(i<now-1)
+            {
+                sentence=sentence+nouns[Math.floor(Math.random()*nouns.length)]+' ';
+            }
+            else{
+                sentence=sentence+nouns[Math.floor(Math.random()*nouns.length)]+'.';
+            }
+        }
+        sentence[0].toUpperCase();
+        return sentence;
+    }
+    else{
+        return 'ERORR 69420!!! Please try again!';
+    }
+    
+}
+createMessage(numOfWords());
+const logMessage=()=>{
+    console.log(createMessage(numOfWords()));
+}
+logMessage();
